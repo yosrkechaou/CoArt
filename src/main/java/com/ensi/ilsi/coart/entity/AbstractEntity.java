b4c670c20197edc8ef224f340b21e8d76a7948ca
aklom <molkazaw@gmail.com>
2018-11-20 18:44:51 +0100

    AbstractEntity

--- src/main/java/com/ensi/ilsi/coart/entity/AbstractEntity.java
+++ src/main/java/com/ensi/ilsi/coart/entity/AbstractEntity.java
@@ -6,7 +6,9 @@
 package com.ensi.ilsi.coart.entity;
 
 import java.io.*;
+import java.time.Instant;
 import javax.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
 
 /**
  *
@@ -14,8 +16,33 @@ import javax.persistence.*;
  */
 public abstract class AbstractEntity implements Serializable {
     
+    private static final long serialVersionUID = 2L; 
+    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id; 
+    
+    
+    @CreatedDate
+    @Column(name = "created_date", nullable = false)
+    private Instant createdDate = Instant.now(); 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+    
+    
     
 }
